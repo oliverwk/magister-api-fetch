1fd8985b6b216ee4cdd37a1bd079730e17d3e020
oliverwk <30601311+oliverwk@users.noreply.github.com>
2020-09-10 17:44:19 +0200

    Add files via upload

--- grafieken.js
+++ grafieken.js
@@ -1,10 +1,30 @@
 const fetch = require('node-fetch');
 require('dotenv').config();
+console.log(process.env.magister_token);
 async function main() {
-      const ress = await fetch('https://niftarlake.magister.net/api/personen/15265/afspraken?van=2020-09-06T22:00:00.0000000Z&tot=2020-09-19T22:00:00.0000000Z',{headers: { 'api-version': '1.0' , 'x-api-client-id': 'EF15', 'user-agent': 'M6LOAPP/2.3.2.272 (iPhone; iOS 13.6.1)', 'authorization':'Bearer '+process.env.mag_token}})
-      let res = await ress.json()
-      console.log(res);
-      console.log(res["Items"]["LesuurTotMet"]);
+      const ress = await fetch('https://niftarlake.magister.net/api/personen/15265/afspraken?van=2020-09-06T22:00:00.0000000Z&tot=2020-09-19T22:00:00.0000000Z',{headers: { 'api-version': '1.0' , 'x-api-client-id': 'EF15', 'user-agent': 'M6LOAPP/2.3.2.272 (iPhone; iOS 13.6.1)', 'authorization':'Bearer '+process.env.magister_token}})
+      if (ress.json()== "SecurityToken Expired") {
+        console.log("SecurityToken Expired");
+          const formData = new FormData();
+          formData.append('refresh_token', process.env.magister_refresh_token);
+          formData.append('client_id', "M6LOAPP");
+          formData.append('grant_type', "refresh_token");
+          const sectokens = await fetch("https://accounts.magister.net/connect/token",{method : "POST",  body: formData, headers: {'user-agent':'M6LOAPP/2.3.2.272 (iPhone; iOS 13.6.1)','content-type':'application/x-www-form-urlencoded','x-api-client-id':'EF15','api-version':'1.0'} })
+          let new_json = await sectokens.json()
+          console.log(new_json);
+          fs = require('fs');
+          fs.writeFile('.env', "magister_token="+new_json["access_token"]+"\n magister_refresh_token="+new_json["refresh_token"], function (err) {
+            if (err) return console.log(err);
+            console.log('Hmagister toekn > .env');
+          });
+          }
+      else {
+        const ressing = await fetch('https://niftarlake.magister.net/api/personen/15265/afspraken?van=2020-09-06T22:00:00.0000000Z&tot=2020-09-19T22:00:00.0000000Z',{headers: { 'api-version': '1.0' , 'x-api-client-id': 'EF15', 'user-agent': 'M6LOAPP/2.3.2.272 (iPhone; iOS 13.6.1)', 'authorization':'Bearer '+process.env.magister_token}})
+        let res = await ressing.json()
+        for (var i = 0; i < res["Items"].length; i++) {
+          console.log( res["Items"][i]["Omschrijving"]);
+        }
+      }
 }
 main()
 const bulshit =`2020-09-07 20:26:36 GET https://niftarlake.magister.net/api/personen/15265/afspraken?van=2020-09-06T22:00:00.0000000Z&tot=2020-09-19T22:00:00.0000000Z HTTP/2.0
@@ -21,4 +41,6 @@ authorization:    Bearer <--token here process.env.mag_token-->
 accept-encoding:  gzip, deflate, br
 Query                                                                                                                                                                                             [m:auto]
 van: 2020-09-06T22:00:00.0000000Z
-tot: 2020-09-19T22:00:00.0000000Z`
+tot: 2020-09-19T22:00:00.0000000Z
+
+`
